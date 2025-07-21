fa90e05976e1afa0f8a22a6a70b849bca9bebf50
Yuchan-Kim <yuchankim0906@gmail.com>
2025-07-21 13:25:35 +0900

    config: docker 파일 작성 완료 및 테스트 완료 (콘솔확인 가능)

--- src/main/java/com/powervoice/kafka_consumer/kafka/MultiTopicConsumer.java
+++ src/main/java/com/powervoice/kafka_consumer/kafka/MultiTopicConsumer.java
@@ -8,22 +8,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class MultiTopicConsumer {
 
-    @KafkaListener(topics = "topic-call", groupId = "consumer-group-id")
+    @KafkaListener(topics = "topic-call")
     public void listenCall(String message) {
         log.info("[topic-call] Received message: {}", message);
     }
 
-    @KafkaListener(topics = "topic-mask", groupId = "consumer-group-id")
+    @KafkaListener(topics = "topic-mask")
     public void listenMask(String message) {
         log.info("[topic-mask] Received message: {}", message);
     }
 
-    @KafkaListener(topics = "topic-result", groupId = "consumer-group-id")
+    @KafkaListener(topics = "topic-result")
     public void listenResult(String message) {
         log.info("[topic-result] Received message: {}", message);
     }
 
-    @KafkaListener(topics = "topic-notify", groupId = "consumer-group-id")
+    @KafkaListener(topics = "topic-notify")
     public void listenNotify(String message) {
         log.info("[topic-notify] Received message: {}", message);
     }
